078419171291db29cbaf55d09a2433aa650fb65f
Nicoogf <nicolasgfalabella@gmail.com>
2023-05-31 12:30:49 -0300

    Configuracion de Teclas

--- script.js
+++ script.js
@@ -11,11 +11,43 @@ const changeFoodPosition = () =>{
 
 
 let foodX  , foodY ; 
+let snakeX  = 5, snakeY= 10 ; 
+let velocityX = 0 , velocityY = 0;
+
+const changueDirection = (e) =>{
+
+    //Cambia el valor de la velocidad con la pulsación de tecla
+
+    if(e.key === "ArrowUp"){
+        velocityX = -1;
+        velocityY =  0;
+    }else if(e.key === "ArrowRight"){    // right
+        velocityX = 0;
+        velocityY = 1;
+    }else if(e.key === "ArrowLeft"){     //left
+        velocityX = 0;
+        velocityY = -1;
+    }else if(e.key === "ArrowDown"){   // down
+        velocityX = 1;
+        velocityY = 0;
+    }
+    initGame()
+}
 
 const initGame = () =>{
     let  htmlMarkup = `<div class="food" style="grid-area:${foodY} / ${foodX}"> </div>`
+
+    //Se actualiza la posicion del snake
+
+    snakeX += velocityX;    
+    snakeY += velocityY;
+
+    htmlMarkup += `<div class="head" style="grid-area:${snakeX} / ${snakeY}"> </div>`
     playBoard.innerHTML =  htmlMarkup;
 }
 
 changeFoodPosition();
 initGame();
+
+
+document.addEventListener("keydown" , changueDirection)
