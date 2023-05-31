5b6e6415af32ceff930b73429939b56a3280a244
Nicoogf <nicolasgfalabella@gmail.com>
2023-05-31 12:10:53 -0300

    Se crea funcion para que el punto aparezca aleatoriamente

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 
 const playBoard = document.querySelector(".play-board")
 
+const changeFoodPosition = () =>{
 
-let foodX = 13 , foodY= 10 ; 
+    //Pasa al azar un valor entre 0 y 30 para la comida 
+
+    foodX= Math.floor(Math.random() * 30) + 1 ;
+    foodY= Math.floor(Math.random() * 30) + 1 ;
+}
+
+
+let foodX  , foodY ; 
 
 const initGame = () =>{
     let  htmlMarkup = `<div class="food" style="grid-area:${foodY} / ${foodX}"> </div>`
     playBoard.innerHTML =  htmlMarkup;
 }
 
+changeFoodPosition();
 initGame();
